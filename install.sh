5d7cd1f5faf4893af3d251e73ecda473fa1d4088
Wei5hang <weishang.lu@colegiovascodagama.eu>
2022-02-13 18:54:11 +0000

    Added install.sh

--- install.sh
+++ install.sh
@@ -11,30 +11,11 @@ if [[ -f "./installed" ]]; then
     echo "Starting FRP server"
     ./frps -c ./config.ini
 else
-    echo "Downloading files for PteroVM"
-    curl -sSLo ptero-vm.zip https://cdn2.mythicalkitten.com/pterodactylmarket/ptero-vm/ptero-vm.zip
-    curl -sSLo apth https://cdn2.mythicalkitten.com/pterodactylmarket/ptero-vm/apth
-    curl -sSLo unzip https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/unzip
-    chmod +x apth
-    echo "Installing the files"
-    ./apth unzip >/dev/null 
-    linux/usr/bin/unzip ptero-vm.zip
-    linux/usr/bin/unzip root.zip
-    tar -xf root.tar.gz 
-    chmod +x ./dist/proot
-    rm -rf ptero-vm.zip
-    rm -rf root.zip
-    rm -rf root.tar.gz
+    echo "Downloading files FRP server"
+    curl -sSLo frps https://github.com/wynn-dev/frp-pterodactyl/raw/main/bin/frps
+    curl -sSLo config.ini https://raw.githubusercontent.com/wynn-dev/frp-pterodactyl/main/bin/config.ini
+    echo "Installing FRP server"
     touch installed
-    ./dist/proot -S . /bin/bash -c "mv apth /usr/bin/"
-    ./dist/proot -S . /bin/bash -c "mv unzip /usr/bin/"
-    ./dist/proot -S . /bin/bash -c "apt-get update"
-    ./dist/proot -S . /bin/bash -c "apt-get -y upgrade"
-    ./dist/proot -S . /bin/bash -c "apt-get -y install curl"
-    ./dist/proot -S . /bin/bash -c "apt-get -y install wget"
-    ./dist/proot -S . /bin/bash -c "apt-get -y install neofetch"
-    ./dist/proot -S . /bin/bash -c "curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py"
-    ./dist/proot -S . /bin/bash -c "chmod +x /bin/systemctl"
     echo "Starting PteroVM"
-    ./dist/proot -S . /bin/bash --login
+    ./frps -c ./config.ini
 fi
